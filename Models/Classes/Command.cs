4c4ef6275c8e1774587553a8ab2fe5de6ad54ede
Cavad_zn25 <Cavad_zn25@STHQ0124-03>
2023-12-12 09:38:03 +0400

    Add project files.

---
+++ Models/Classes/Command.cs
@@ -0,0 +1,22 @@
+﻿using Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Classes
+{
+    [Serializable]
+    public class Command
+    {
+
+        // Properties
+
+        public int Id { get; set; }
+        public Car Car { get; set; }
+        public HttpMethods Method { get; set; }
+    }
+}
